b308f21b29c83e9b9d64a9d2e58fab64f5e64336
Rise <64920400+frizerrer@users.noreply.github.com>
2025-05-10 16:37:36 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ComputerApp
+{
+    public class Computer
+    {
+        
+        public int InventoryNumber;
+        public double ProcessorFrequencyGHz;
+        public int HardDriveSizeGB;
+
+       
+        public void ShowInfo()
+        {
+            Console.WriteLine("Инвентарный номер: " + InventoryNumber);
+            Console.WriteLine("Частота процессора: " + ProcessorFrequencyGHz + " ГГц");
+            Console.WriteLine("Объём жёсткого диска: " + HardDriveSizeGB + " ГБ");
+        }
+
+        
+        public double CalculateMemoryCost(double costPerGB)
+        {
+            return HardDriveSizeGB * costPerGB;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Computer comp = new Computer();
+
+            
+            comp.InventoryNumber = 123345;
+            comp.ProcessorFrequencyGHz = 1.5;
+            comp.HardDriveSizeGB = 400;
+
+            
+            comp.ShowInfo();
+
+            double costPerGB = 100;
+            double totalCost = comp.CalculateMemoryCost(costPerGB);
+
+            Console.WriteLine("Стоимость внешней памяти: " + totalCost + " рублей");
+        }
+    }
+}
